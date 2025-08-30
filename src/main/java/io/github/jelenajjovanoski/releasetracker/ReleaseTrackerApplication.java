167786485e69f99dea2677052ebeedb115257f0a
Jelena Jovanoski <jelenajjovanoski@gmail.com>
2025-08-30 11:15:18 +0200

    Initial commit - Spring Boot app with pom.xml and Liquibase changelogs

---
+++ src/main/java/io/github/jelenajjovanoski/releasetracker/ReleaseTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.github.jelenajjovanoski.releasetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jelenajjovanoski/releasetracker/ReleaseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jelenajjovanoski.releasetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
